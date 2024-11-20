7c91d76a7a0313c894c48978564bae4c14e89ff2
Piotrek <kebs878@gmail.com>
2024-11-20 18:06:08 +0100

    fixed issue_3

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 import config
+from player import Player
 
 class Game:
     def __init__(self) -> None:
@@ -9,6 +10,8 @@ class Game:
 
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         pygame.display.set_caption("GAME 1")
+
+        self.player = Player(0, self.screen_height // 2, config.PLAYER_WIDTH, config.PLAYER_HEIGHT, config.YELLOW)
     
     def event_loop(self) -> None:
         for event in pygame.event.get():
@@ -18,5 +21,7 @@ class Game:
     def run(self) -> None:
         while self.running == True:
             self.screen.fill(config.WHITE)
+            self.player.draw(self.screen)
             self.event_loop()
             pygame.display.flip()
+
