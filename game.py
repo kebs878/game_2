7b5409227e9261fe8c920090220e49d168fab928
Piotrek <kebs878@gmail.com>
2024-11-25 17:18:33 +0100

    changed

--- game.py
+++ game.py
@@ -9,8 +9,10 @@ class Game:
         self.screen_width = config.SCREEN_WIDTH
         self.screen_height = config.SCREEN_HEIGHT
         self.font = pygame.font.Font(None, config.FONT_SIZE)
+
+        self.reset_game()
+
         self.running = True
-        self.game_over = False
 
         self.enemy_1 = Enemy(200, 100, 30, 30, config.RED)
         self.enemy_2 = Enemy(600, 50, 30, 30, config.YELLOW)
@@ -29,10 +31,20 @@ class Game:
             config.PLAYER_SPEED,
         )
 
+    def reset_game(self):
+        self.game_over = False
+        self.enemy_1 = Enemy(200, 100, 30, 30, config.RED)
+        self.enemy_2 = Enemy(600, 50, 30, 30, config.YELLOW)
+        self.enemy_3 = Enemy(350, 75, 30, 30, config.BLACK)
+        self.enemies = [self.enemy_1 , self.enemy_2, self.enemy_3]
+
     def event_loop(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            if self.game_over and event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    self.reset_game()
 
     def move_bullets(self) -> None:
         for bullet in self.player.shots:
@@ -46,6 +58,7 @@ class Game:
             for bullet in self.player.shots:
                 if enemy.get_rect().colliderect(bullet.get_rect()):
                     self.enemies.remove(enemy)
+                    print(self.enemy_1)
                     self.player.shots.remove(bullet)
                 if self.enemies == []:
                     self.game_over = True
